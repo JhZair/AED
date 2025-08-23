f85e2525ec6140073ab3b1bf18b4bea973a38bb6
JhZair <98540197+JhZair@users.noreply.github.com>
2025-08-23 11:18:36 -0500

    Add files via upload

---
+++ Ejercicios_Gustavo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Encontrar_Horizontal(int * a, vector<int> p){
+	for(int * i = a;a + i < a + p.size();i++){			//como se puede iterar usando solo punteros
+		if(*i == 
+	}
+}
+
+int main(int argc, char *argv[]) {
+	
+	vector<int> vect(100);
+	for(auto i : vect){
+		if(i == 'P'){
+			Encontrar_Horizontal(&i+1)
+		}
+	}
+	
+	
+	return 0;
+}
+
+//encontrar ocurrencias de palabras patan, tapa, pato, tipa
+// solo horizontal y verticalmente
+
+// hacer programa que encuentre el camino mas corto en un laberinto
