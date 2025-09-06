31d36c0db239fbb4b0fa78b92e918ba9f55fb074
JhZair <johnzairoros@gmail.com>
2025-09-05 20:20:59 -0500

    cambio en ejercicio

--- Ejercicios_Gustavo.cpp
+++ Ejercicios_Gustavo.cpp
@@ -5,36 +5,38 @@
 
 // hacer un buscaminas 10x10 usando matrices y punteros una version con threads y otra sin
 
-#include <iostream>
-#include <string>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// using namespace std;
 
-// int Encontrar_Horizontal(string palabra, char b[][10],int tam_pal){
-// 	int res=0;
-// 	char * pal = &palabra[0];
-// 	for(char * p = *b; p < *b + 100;p++){
-// 		if(*p == *pal){
-// 			pal++;
-// 		}
-// 		if(*pal == '\0'){
-// 			res++;
-// 			pal = &palabra[0];
-// 		}
-// 		else{
-// 		pal = &palabra[0];
-// 		}
-// 	}
-// 	return res;
+
+// int contarOcurrenciasHorizontales(char matriz[10][10], string palabra) {
+//     int res = 0;
+//     int largoPalabra = palabra.length();
+//     for (int i = 0; i < 10; ++i) {
+//         for (int j = 0; j <= 10 - largoPalabra; ++j) {
+//             bool palabraEncontrada = true;
+//             for (int k = 0; k < largoPalabra; ++k) {
+//                 if (matriz[i][j + k] != palabra[k]) {
+//                     palabraEncontrada = false;
+//                     break; 
+//                 }
+//             }
+//             if (palabraEncontrada) {
+//                 res++;
+//             }
+//         }
+//     }
+//     return res;
 // }
 
-// int main(int argc, char *argv[]) {
-
-// 	char tabla[10][10]= {
+// int main() {
+//     char tabla[10][10] = {
 //         {'T', 'P', 'P', 'A', 'P', 'T', 'P', 'Z', 'R', 'T'},
 //         {'O', 'P', 'A', 'T', 'A', 'P', 'O', 'Z', 'O', 'J'},
 //         {'S', 'A', 'T', 'A', 'T', 'O', 'T', 'A', 'Z', 'I'},
-//         {'A', 'T', 'A', 'P', 'A', 'D', 'A', 'T', 'O', 'S'},
+//         {'A', 'T', 'A', 'P', 'A', 'P', 'A', 'T', 'O', 'S'},
 //         {'P', 'A', 'T', 'A', 'N', 'O', 'P', 'O', 'T', 'N'},
 //         {'T', 'A', 'A', 'P', 'A', 'T', 'I', 'P', 'A', 'P'},
 //         {'R', 'R', 'N', 'A', 'D', 'O', 'T', 'A', 'D', 'O'},
@@ -42,54 +44,92 @@ using namespace std;
 //         {'N', 'I', 'I', 'A', 'T', 'I', 'T', 'A', 'P', 'A'},
 //         {'P', 'S', 'S', 'N', 'T', 'T', 'T', 'P', 'A', 'P'}
 //     };
-// 	const int tam = 5;
-// 	string palabras[tam] = {"PATAN","TIPA","PATA","PATO","TAPA"};
-// 	for(string *p = palabras; p < palabras + tam;p++){
-// 		cout <<*p << " tiene "<< Encontrar_Horizontal(*p, tabla, (*p).size()) << " ocurrencias"<< endl;	
-// 	}
 
-// 	return 0;
+//     vector<string> palabras = {"PATAN", "TIPA", "PATA", "PATO", "TAPA"};
+
+//     for (string p : palabras) {
+//         cout << p << " tiene " << contarOcurrenciasHorizontales(tabla, p) << " ocurrencias" << endl;
+//     }
+
+//     return 0;
 // }
 
+#include <iostream>
+#include <string>
+#include <vector>
 
-int contarOcurrenciasHorizontales(char matriz[10][10], string palabra) {
-    int res = 0;
-    int largoPalabra = palabra.length();
-    for (int i = 0; i < 10; ++i) {
-        for (int j = 0; j <= 10 - largoPalabra; ++j) {
-            bool palabraEncontrada = true;
-            for (int k = 0; k < largoPalabra; ++k) {
-                if (matriz[i][j + k] != palabra[k]) {
-                    palabraEncontrada = false;
-                    break; 
+int contarOcurrencias(const char matriz[10][10], const std::string& palabra) {
+    int contador = 0;
+    const size_t largoPalabra = palabra.length();
+    if (largoPalabra == 0 || largoPalabra > 10) {
+        return 0;
+    }
+
+    const char* p_palabra_fin = &palabra[0] + largoPalabra;
+
+    // Búsqueda Horizontal
+    const char (*p_fila_fin_h)[10] = matriz + 10;
+    for (const char (*p_fila)[10] = matriz; p_fila < p_fila_fin_h; ++p_fila) {
+        const char* p_matriz_limite_inicio = *p_fila + 10 - largoPalabra;
+        for (const char* p_matriz_inicio = *p_fila; p_matriz_inicio <= p_matriz_limite_inicio; ++p_matriz_inicio) {
+            bool encontrada = true;
+            const char* p_matriz = p_matriz_inicio;
+            for (const char* p_palabra = &palabra[0]; p_palabra < p_palabra_fin; ++p_palabra, ++p_matriz) {
+                if (*p_matriz != *p_palabra) {
+                    encontrada = false;
+                    break;
                 }
             }
-            if (palabraEncontrada) {
-                res++;
+            if (encontrada) {
+                contador++;
             }
         }
     }
-    return res;
+
+    // Búsqueda Vertical
+    const char (*p_fila_limite_inicio_v)[10] = matriz + 10 - largoPalabra;
+    for (const char (*p_fila)[10] = matriz; p_fila <= p_fila_limite_inicio_v; ++p_fila) {
+        const char* p_columna_fin = *p_fila + 10;
+        for (const char* p_columna_inicio = *p_fila; p_columna_inicio < p_columna_fin; ++p_columna_inicio) {
+            bool encontrada = true;
+            const char* p_matriz = p_columna_inicio;
+            for (const char* p_palabra = &palabra[0]; p_palabra < p_palabra_fin; ++p_palabra) {
+                if (*p_matriz != *p_palabra) {
+                    encontrada = false;
+                    break;
+                }
+                p_matriz += 10;
+            }
+            if (encontrada) {
+                contador++;
+            }
+        }
+    }
+
+    return contador;
 }
 
+
 int main() {
+    using namespace std;
+
     char tabla[10][10] = {
-        {'T', 'P', 'P', 'A', 'P', 'T', 'P', 'Z', 'R', 'T'},
-        {'O', 'P', 'A', 'T', 'A', 'P', 'O', 'Z', 'O', 'J'},
-        {'S', 'A', 'T', 'A', 'T', 'O', 'T', 'A', 'Z', 'I'},
-        {'A', 'T', 'A', 'P', 'A', 'P', 'A', 'T', 'O', 'S'},
-        {'P', 'A', 'T', 'A', 'N', 'O', 'P', 'O', 'T', 'N'},
-        {'T', 'A', 'A', 'P', 'A', 'T', 'I', 'P', 'A', 'P'},
-        {'R', 'R', 'N', 'A', 'D', 'O', 'T', 'A', 'D', 'O'},
-        {'E', 'D', 'N', 'T', 'A', 'P', 'O', 'T', 'A', 'T'},
-        {'N', 'I', 'I', 'A', 'T', 'I', 'T', 'A', 'P', 'A'},
-        {'P', 'S', 'S', 'N', 'T', 'T', 'T', 'P', 'A', 'P'}
+        {'T','P','P','A','P','T','P','Z','R','T'},
+        {'O','P','A','T','A','P','O','Z','O','J'},
+        {'S','A','T','A','T','O','T','A','Z','I'},
+        {'A','T','A','P','A','P','A','T','O','S'},
+        {'P','A','T','A','N','O','P','O','T','N'},
+        {'T','A','A','P','A','T','I','P','A','P'},
+        {'R','R','N','A','D','O','T','A','D','O'},
+        {'E','D','N','T','A','P','O','T','A','T'},
+        {'N','I','I','A','T','I','T','A','P','A'},
+        {'P','S','S','N','T','T','T','P','A','P'}
     };
 
     vector<string> palabras = {"PATAN", "TIPA", "PATA", "PATO", "TAPA"};
 
-    for (string p : palabras) {
-        cout << p << " tiene " << contarOcurrenciasHorizontales(tabla, p) << " ocurrencias" << endl;
+    for (string& p : palabras) {
+        cout << p << " tiene " << contarOcurrencias(tabla, p) << " ocurrencias" << endl;
     }
 
     return 0;
