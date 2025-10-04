13714bd4738adb07033836edf67461459c0ab62d
JhZair <98540197+JhZair@users.noreply.github.com>
2025-10-04 11:29:16 -0500

    Create Btree.cpp

---
+++ Btree.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+	int v;
+	Node * right;
+	Node * left;
+	Node(){
+		v = 0;
+		right = left = 0;
+	}
+};
+
+class Btree{
+	Node * root;
+//	int nelemns
+public:
+	Btree();
+	~Btree();
+	bool fnd(int x, Node**& p);
+	bool ins(int x, Node**& p);
+	bool remv(int x, Node**& p);
+	void imprimir();
+	
+};
+
+Btree::Btree(){
+	root = 0;
+}
+Btree::~Btree(){
+	
+}
+
+bool Btree::fnd(int x, Node**& p){
+	p = &root;
+	while(*p && (*p)->v != x){
+		if((*p)->v < x){
+			p = &((*p)->left));
+		}
+		else{
+			p = &((*p)->right));
+		}
+	}
+	return (*p) != 0;
+}
+bool Btree::ins(int x, Node**& p){
+	
+}
+bool Btree::remv(int x, Node**& p){}
+void Btree::imprimir(){}
+
+
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
+
+//hay tarea 
