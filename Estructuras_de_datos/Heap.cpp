8f6120f65a5278a0aa61f4927b467900041de365
JhZair <98540197+JhZair@users.noreply.github.com>
2025-11-22 11:06:46 -0500

    Add initial Heap class implementation
    Implement basic Heap class with push method and main function.

---
+++ Estructuras_de_datos/Heap.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Heap{
+	vector<int> heap;
+public:
+	Heap();
+	void push(int x){
+		vector.push_back(x);
+		int index = vector.size()-1;
+	}
+	void rmv(int x){
+		
+	}
+	void print(){
+		
+	}
+};
+
+int main(int argc, char *argv[]) {
+	Heap h;
+	heap.push(5);heap.print();
+	heap.push(15);heap.print();
+	heap.push(12);heap.print();
+	heap.push(2);heap.print();
+
+	
+	return 0;
+}
+
